8fcc9887d6ad895d1a29098997719469d1229f9d
eugene.yurchenko <jackyurchenco@gmail.com>
2026-03-31 17:30:38 +0200

    refactor(prisma): move exception filter registration to PrismaModule

--- libs/shared/prisma/src/lib/prisma-client-exception.filter.ts
+++ libs/shared/prisma/src/lib/prisma-client-exception.filter.ts
@@ -1,10 +1,13 @@
 import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
-import { BaseExceptionFilter } from '@nestjs/core';
+import { BaseExceptionFilter, HttpAdapterHost } from '@nestjs/core';
 import { Prisma } from '@prisma/client';
 import { Response } from 'express';
 
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  constructor(httpAdapterHost: HttpAdapterHost) {
+    super(httpAdapterHost.httpAdapter);
+  }
   override catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
--- libs/shared/prisma/src/lib/prisma.module.ts
+++ libs/shared/prisma/src/lib/prisma.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from "./prisma.service";
+import { APP_FILTER } from "@nestjs/core";
+import { PrismaClientExceptionFilter } from "./prisma-client-exception.filter";
 
 @Module({
   controllers: [],
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_FILTER,
+      useClass: PrismaClientExceptionFilter,
+    },
+  ],
   exports: [PrismaService],
 })
 export class PrismaModule {}
