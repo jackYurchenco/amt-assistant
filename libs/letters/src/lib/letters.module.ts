d6a8755b7694e4a605170895b865517a6106cbe2
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-01 15:31:14 +0200

    chore(letters): add GetLetter and GetLettersByUserId use cases to LettersModule

--- libs/letters/src/lib/letters.module.ts
+++ libs/letters/src/lib/letters.module.ts
@@ -4,6 +4,8 @@ import { CreateLetterUseCase } from "./application/create-letter/create-letter.u
 import { PrismaLetterRepository } from "./infrastructure/prisma-letter.repository";
 import { LettersController } from "./interface/letters.controller";
 import { ILetterRepository } from "./domain/letter.repository.interface";
+import { GetLetterUseCase } from "./application/get-letter/get-letter.use-case";
+import { GetLettersByUserIdUseCase } from "./application/get-letters-by-user-id/get-letters-by-user-id.use-case";
 
 
 @Module({
@@ -11,11 +13,12 @@ import { ILetterRepository } from "./domain/letter.repository.interface";
   controllers: [LettersController],
   providers: [
     CreateLetterUseCase,
+    GetLetterUseCase,
+    GetLettersByUserIdUseCase,
     {
       provide: ILetterRepository,
       useClass: PrismaLetterRepository,
     }
-  ],
-  exports: [CreateLetterUseCase],
+  ]
 })
 export class LettersModule {}
