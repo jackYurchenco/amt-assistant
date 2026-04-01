4a50728d7fd8a0c732677007bc7919a4063192c8
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-01 13:47:39 +0200

    feat(letters): implement get letter by id endpoint

--- libs/letters/src/lib/interface/letters.controller.ts
+++ libs/letters/src/lib/interface/letters.controller.ts
@@ -1,12 +1,17 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, NotFoundException, Param, Post } from "@nestjs/common";
 import { CreateLetterUseCase } from "../application/create-letter/create-letter.use-case";
 import { ApiOperation, ApiResponse } from "@nestjs/swagger";
 import { CreateLetterDto } from "./dto/create-letter.dto";
 import { CreateLetterCommand } from "../application/create-letter/create-letter.command";
+import { GetLetterDto } from "./dto/get-letter.dto";
+import { GetLetterUseCase } from "../application/get-letter/get-letter.use-case";
 
 @Controller('letters')
 export class LettersController {
-  constructor(private readonly createLetterUseCase: CreateLetterUseCase) {}
+  constructor(
+    private readonly createLetterUseCase: CreateLetterUseCase,
+    private readonly getLetterUseCase: GetLetterUseCase,
+  ) {}
 
   @Post()
   @ApiOperation({ summary: 'Create a new letter' })
@@ -27,5 +32,20 @@ export class LettersController {
     )
 
     return await this.createLetterUseCase.execute(command);
+  }
+
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get a specific letter by ID' })
+  @ApiResponse({ status: 200, description: 'Letter found successfully' })
+  @ApiResponse({ status: 404, description: 'Letter not found' })
+  async findOne(@Param() params: GetLetterDto) {
+    const letter = await this.getLetterUseCase.execute({ id: params.id });
+
+    if (!letter) {
+      throw new NotFoundException(`Letter with ID ${params.id} not found`);
+    }
+
+    return letter;
   }
 }
