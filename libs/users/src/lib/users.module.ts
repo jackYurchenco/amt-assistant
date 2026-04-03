fb189f4bdb782d2d9cdb321953dbbc661657e4f9
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-03 09:48:00 +0200

    feat(users): add UsersController and register in module

--- libs/users/src/lib/users.module.ts
+++ libs/users/src/lib/users.module.ts
@@ -3,9 +3,10 @@ import { IUserRepository } from "./domain/user.repository.interface";
 import { PrismaUserRepository } from "./infrastructure/prisma-user.repository";
 import { GetUserByIdUseCase } from "./application/get-user-by-id/get-user-by-id.use-case";
 import { GetAllUsersUseCase } from "./application/get-all-users/get-all-users.use-case";
+import { UsersController } from "./interface/users.controller";
 
 @Module({
-  controllers: [],
+  controllers: [UsersController],
   providers: [
     {
       provide: IUserRepository,
