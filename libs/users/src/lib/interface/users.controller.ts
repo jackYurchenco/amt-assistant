8f12dac6ac10f2f5947ade0aef3dd4f732fee889
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-03 10:04:39 +0200

    feat(users): implement create user endpoint

--- libs/users/src/lib/interface/users.controller.ts
+++ libs/users/src/lib/interface/users.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreateUserUseCase } from "../application/create-user/create-user.use-case";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { CreateUserCommand } from "../application/create-user/create-user.command";
 
+@ApiTags('users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    private readonly createUserUseCase: CreateUserUseCase,
+  ) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully created.'
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid input data.'
+  })
+  async create(@Body() dto: CreateUserDto) {
+    const command = new CreateUserCommand(
+      dto.email,
+      dto.passwordHash
+    );
+    return await this.createUserUseCase.execute(command);
+  }
+}
