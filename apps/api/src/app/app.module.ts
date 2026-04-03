14efa4bd2c974d82dfa469e302c73065f17fc85c
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-03 09:51:18 +0200

    feat(api): register UsersModule in AppModule

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from "@amt-assistant/prisma";
 import { LettersModule } from "@amt-assistant/letters";
+import { UsersModule } from "@amt-assistant/users";
 
 @Module({
   imports: [
     PrismaModule,
-    LettersModule
+    LettersModule,
+    UsersModule
   ],
   controllers: [],
   providers: [],
