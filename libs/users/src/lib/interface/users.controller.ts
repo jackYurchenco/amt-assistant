1e8792306412e27eadfe8f41dc02be27195bafd4
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-04 09:46:06 +0200

    feat(users): implement get all users endpoint

--- libs/users/src/lib/interface/users.controller.ts
+++ libs/users/src/lib/interface/users.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { CreateUserUseCase } from "../application/create-user/create-user.use-case";
 import { GetUserByIdUseCase } from "../application/get-user-by-id/get-user-by-id.use-case";
+import { GetAllUsersUseCase } from "../application/get-all-users/get-all-users.use-case";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { CreateUserCommand } from "../application/create-user/create-user.command";
@@ -13,6 +14,7 @@ export class UsersController {
   constructor(
     private readonly createUserUseCase: CreateUserUseCase,
     private readonly getUserByIdUseCase: GetUserByIdUseCase,
+    private readonly getAllUsersUseCase: GetAllUsersUseCase,
   ) {}
 
   @Post()
@@ -31,6 +33,17 @@ export class UsersController {
       dto.passwordHash
     );
     return await this.createUserUseCase.execute(command);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({
+    status: 200,
+    description: 'The users have been successfully retrieved.',
+    type: [User]
+  })
+  async findAll(): Promise<User[]> {
+    return await this.getAllUsersUseCase.execute();
   }
 
   @Get(':id')
