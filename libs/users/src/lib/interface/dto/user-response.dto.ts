ba89347d2f0cd48733a4c00ae5c06bf04b538a16
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-07 16:35:26 +0200

    feat(users): add user response dto

---
+++ libs/users/src/lib/interface/dto/user-response.dto.ts
@@ -0,0 +1,26 @@
+import { IUser } from '@amt-assistant/contracts';
+import { User } from '../../domain/user.entity';
+
+export class UserResponseDto implements IUser {
+  id: string;
+  email: string;
+  passwordHash: string;
+  createdAt: Date;
+  updatedAt: Date;
+  firstName?: string | null;
+  lastName?: string | null;
+
+  private constructor(user: User) {
+    this.id = user.id;
+    this.email = user.email;
+    this.passwordHash = user.passwordHash;
+    this.createdAt = user.createdAt;
+    this.updatedAt = user.updatedAt;
+    this.firstName = user.firstName ?? null;
+    this.lastName = user.lastName ?? null;
+  }
+
+  static fromEntity(user: User): UserResponseDto {
+    return new UserResponseDto(user);
+  }
+}
