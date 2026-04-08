d266f1920fec7e1ce9b2c2d2c6849c00ec0f0f81
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-08 17:52:00 +0200

    refactor(auth): restructure library according to DDD principles

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { AuthController } from './interface/auth.controller';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService],
+  providers: [],
+  exports: [],
 })
 export class AuthModule {}
