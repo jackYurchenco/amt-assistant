fc1a72e73616e5f0818365146b2aa7c132dded45
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-10 19:28:38 +0200

    chore(util-crypto): provide and export HasherService

--- libs/shared/util-crypto/src/lib/util-crypto.module.ts
+++ libs/shared/util-crypto/src/lib/util-crypto.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { HasherService } from './hasher-service';
+import { BcryptHasherService } from './bcrypt-hasher.service';
 
 @Module({
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [
+    {
+      provide: HasherService,
+      useClass: BcryptHasherService,
+    },
+  ],
+  exports: [HasherService],
 })
 export class UtilCryptoModule {}
