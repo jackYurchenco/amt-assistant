d850f5dfb02e6fb0214df355c9c24122f9700d15
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-16 17:36:57 +0200

    feat(app): register AuthModule in AppModule

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from "@amt-assistant/prisma";
 import { LettersModule } from "@amt-assistant/letters";
 import { UsersModule } from "@amt-assistant/users";
+import { AuthModule } from '@amt-assistant/auth';
 
 @Module({
   imports: [
     PrismaModule,
+    AuthModule,
     LettersModule,
     UsersModule
   ],
