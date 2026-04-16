f548c6386aa5cf0e3983062657fd5e5ea232c661
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-16 18:27:40 +0200

    feat(util-token): implement async JwtModule registration using ConfigService

--- libs/shared/util-token/src/lib/util-token.module.ts
+++ libs/shared/util-token/src/lib/util-token.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { JwtTokenService } from './jwt-token.service';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    JwtModule.register({})
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET') || '',
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
   ],
   providers: [
     {
