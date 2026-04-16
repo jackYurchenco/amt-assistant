4cc7b9df6d87b1585987dd70b31aa45c3c45bb3f
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-16 17:54:41 +0200

    fix(auth): add LoginUseCase to AuthModule providers

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './interface/auth.controller';
 import { UtilCryptoModule } from '@amt-assistant/util-crypto';
 import { UtilTokenModule } from '@amt-assistant/util-token';
+import { LoginUseCase } from './application/login.use-case';
 
 @Module({
   imports: [
@@ -11,7 +12,9 @@ import { UtilTokenModule } from '@amt-assistant/util-token';
   controllers: [
     AuthController
   ],
-  providers: [],
+  providers: [
+    LoginUseCase,
+  ],
   exports: [],
 })
 export class AuthModule {}
