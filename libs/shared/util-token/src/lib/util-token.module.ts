dd256c0b130c11dc672b15ee81464c520eade9ad
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-16 17:42:21 +0200

    fix(util-token): register JwtModule to resolve JwtService dependency

--- libs/shared/util-token/src/lib/util-token.module.ts
+++ libs/shared/util-token/src/lib/util-token.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { JwtTokenService } from './jwt-token.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({})
+  ],
   providers: [
     {
       provide: TokenService,
