941dc8a93a3074d34472d7c48a6c64fcb5467c48
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-22 14:02:45 +0200

    feat(domain): add abstract EntityId value object

--- libs/shared/domain/src/index.ts
+++ libs/shared/domain/src/index.ts
@@ -1,1 +1,2 @@
+export * from './lib/entity-id.value-object';
 export * from './lib/user-id.value-object';
---
+++ libs/shared/domain/src/lib/entity-id.value-object.ts
@@ -0,0 +1,25 @@
+import { validate } from 'uuid';
+
+export abstract class EntityId {
+  protected constructor(protected readonly value: string) {
+    this.validate(value);
+  }
+
+  private validate(id: string): void {
+    if (!id) {
+      throw new Error('UserId cannot be empty');
+    }
+
+    if (!validate(id)) {
+      throw new Error(`Invalid UserId format: ${id}. Expected UUID v4.`);
+    }
+  }
+
+  public getValue(): string {
+    return this.value;
+  }
+
+  public equals(other: EntityId | null | undefined): boolean {
+    return this.value === other?.getValue();
+  }
+}
