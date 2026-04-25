649fd05a85fecc959d696efc9295eb93c8ad74a8
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-25 15:59:39 +0200

    refactor(users): construct value objects for repository queries

--- libs/users/src/lib/application/get-user-by-id/get-user-by-id.use-case.ts
+++ libs/users/src/lib/application/get-user-by-id/get-user-by-id.use-case.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { GetUserByIdQuery } from './get-user-by-id.query';
 import { User } from '../../domain/user.entity';
 import { UserRepository } from '../../domain/user.repository';
+import { UserId } from '@amt-assistant/domain';
 
 @Injectable()
 export class GetUserByIdUseCase {
   constructor(private readonly userRepository: UserRepository) {}
 
   async execute(query: GetUserByIdQuery): Promise<User | null> {
-    return this.userRepository.findById(query.id);
+    return this.userRepository.findById(UserId.create(query.id));
   }
 }
