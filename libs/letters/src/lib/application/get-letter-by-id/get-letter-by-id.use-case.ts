926f175c2e7c00a5b479e9cad9c5cd7b5ba6eac2
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-25 15:57:24 +0200

    refactor(letters): construct value objects for repository queries

--- libs/letters/src/lib/application/get-letter-by-id/get-letter-by-id.use-case.ts
+++ libs/letters/src/lib/application/get-letter-by-id/get-letter-by-id.use-case.ts
@@ -2,13 +2,14 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { GetLetterByIdQuery } from './get-letter-by-id.query';
 import { Letter } from '../../domain/letter.entity';
 import { LetterRepository } from '../../domain/letter.repository';
+import { LetterId } from '@amt-assistant/domain';
 
 @Injectable()
 export class GetLetterByIdUseCase {
   constructor(private readonly letterRepository: LetterRepository) {}
 
   async execute(query: GetLetterByIdQuery): Promise<Letter> {
-    const letter = await this.letterRepository.findById(query.id);
+    const letter = await this.letterRepository.findById(LetterId.create(query.id));
 
     if (!letter) {
       throw new NotFoundException(`Letter with ID ${query.id} not found`);
--- libs/letters/src/lib/application/get-letters-by-user-id/get-letters-by-user-id.use-case.ts
+++ libs/letters/src/lib/application/get-letters-by-user-id/get-letters-by-user-id.use-case.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { Letter } from '../../domain/letter.entity';
 import { GetLettersByUserIdQuery } from './get-letters-by-user-id.query';
 import { LetterRepository } from '../../domain/letter.repository';
+import { UserId } from '@amt-assistant/domain';
 
 @Injectable()
 export class GetLettersByUserIdUseCase {
   constructor(private readonly letterRepository: LetterRepository) {}
 
   async execute(query: GetLettersByUserIdQuery): Promise<Letter[]> {
-    return this.letterRepository.findByUserId(query.userId);
+    return this.letterRepository.findByUserId(UserId.create(query.userId));
   }
 }
