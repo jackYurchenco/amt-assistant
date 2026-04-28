c0f38c4036f99a9a7d44bd1ad3585635a07465a9
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-28 15:09:01 +0200

    feat(users): enhance CreateUserDto validation

--- libs/users/src/lib/interface/dto/create-user.dto.ts
+++ libs/users/src/lib/interface/dto/create-user.dto.ts
@@ -1,17 +1,31 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 import { ICreateUser } from '@amt-assistant/contracts';
+import { Trim } from '@amt-assistant/util-decorators';
 
 export class CreateUserDto implements ICreateUser {
-  @ApiProperty({ example: 'user@example.com', description: 'The email of the user.' })
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'The email of the user.',
+    type: String,
+    required: true,
+  })
   @IsEmail({}, { message: 'Invalid email format.' })
   @IsNotEmpty({ message: 'The email cannot be empty.' })
+  @IsString({ message: 'The email must be a string.' })
+  @Trim()
   email!: string;
 
-  @ApiProperty({ example: 'password123', description: 'The password of the user.' })
-  @IsString({ message: 'Password must be a string.' })
-  @IsNotEmpty({ message: 'The password cannot be empty.' })
-  @MinLength(6, { message: 'The password must be at least 6 characters long.' })
+  @ApiProperty({
+    example: 'password123',
+    description: 'The password of the user.',
+    type: String,
+    required: true,
+  })
   @MaxLength(64, { message: 'The password cannot be longer than 64 characters.' })
+  @MinLength(6, { message: 'The password must be at least 6 characters long.' })
+  @IsNotEmpty({ message: 'The password cannot be empty.' })
+  @IsString({ message: 'Password must be a string.' })
+  @Trim()
   password!: string;
 }
