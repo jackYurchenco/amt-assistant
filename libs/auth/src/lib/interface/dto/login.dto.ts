77bb408d3335b535ffa56bcabb404ca39914c4ad
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-28 15:31:02 +0200

    feat(auth): enhance LoginDto validation

--- libs/auth/src/lib/interface/dto/login.dto.ts
+++ libs/auth/src/lib/interface/dto/login.dto.ts
@@ -1,22 +1,33 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 import { ILogin } from '@amt-assistant/contracts';
 import { ApiProperty } from '@nestjs/swagger';
+import { Trim } from '@amt-assistant/util-decorators';
 
 export class LoginDto implements ILogin {
   @ApiProperty({
     example: 'user@example.com',
     description: 'The email address of the user',
+    type: String,
+    required: true,
   })
-  @IsEmail()
+  @IsEmail({}, { message: 'Invalid email format.' })
+  @IsNotEmpty({ message: 'The email cannot be empty.' })
+  @IsString({ message: 'The email must be a string.' })
+  @Trim()
   email!: string;
 
   @ApiProperty({
     example: 'password123',
-    description: 'The user password (minimum 6 characters)',
-    minLength: 6,
+    description: 'The user password (minimum 6 characters, maximum 64 characters)',
+    type: String,
+    required: true,
     format: 'password',
+    minLength: 6,
+    maxLength: 64,
   })
-  @IsString()
-  @MinLength(6)
+  @MaxLength(64, { message: 'The password cannot be longer than 64 characters.' })
+  @MinLength(6, { message: 'The password must be at least 6 characters long.' })
+  @IsNotEmpty({ message: 'The password cannot be empty.' })
+  @IsString({ message: 'Password must be a string.' })
   password!: string;
 }
