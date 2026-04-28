98851193beba82098164f953a9d9d84140ee8ce4
eugene.yurchenko <jackyurchenco@gmail.com>
2026-04-28 13:54:21 +0200

    feat(letters): enhance CreateLetterDto validation

--- libs/letters/src/lib/interface/dto/create-letter.dto.ts
+++ libs/letters/src/lib/interface/dto/create-letter.dto.ts
@@ -1,14 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 import { ICreateLetter } from '@amt-assistant/contracts';
+import { Transform } from 'class-transformer';
 
 export class CreateLetterDto implements ICreateLetter {
   @ApiProperty({
     example: 'Rechnung für Strom',
     description: 'The subject or title of the letter',
+    required: true,
   })
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'The title must be a string.' })
+  @IsNotEmpty({ message: 'The title cannot be empty.'})
+  @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
   @MinLength(3)
   @MaxLength(100)
   readonly title!: string;
@@ -18,8 +21,9 @@ export class CreateLetterDto implements ICreateLetter {
     description: 'The person or organization who sent the letter',
     required: false,
   })
-  @IsString()
   @IsOptional()
-  @IsNotEmpty()
+  @IsString({ message: 'The sender must be a string.'})
+  @IsNotEmpty({ message: 'The sender cannot be empty.'})
+  @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
   readonly sender?: string;
 }
