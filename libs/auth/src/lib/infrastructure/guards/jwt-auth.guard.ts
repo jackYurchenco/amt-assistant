3dc1c376a91581bd5210a4d70bb39cf246de6ee6
eugene.yurchenko <jackyurchenco@gmail.com>
2026-05-14 22:11:54 +0200

    feat(auth): add public route support to JwtAuthGuard

--- libs/auth/src/lib/infrastructure/guards/jwt-auth.guard.ts
+++ libs/auth/src/lib/infrastructure/guards/jwt-auth.guard.ts
@@ -1,11 +1,29 @@
 import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { IS_PUBLIC_KEY } from '@amt-assistant/util-decorators';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
 
-  handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
+  override canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
 
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+
+  override handleRequest(err: any, user: any, info: any, context: ExecutionContext): any {
     if (err || !user) {
       throw err || new UnauthorizedException('Користувач не авторизований');
     }
