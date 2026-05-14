26fdee7f2bb75591a9c812675913bec6cde317d9
eugene.yurchenko <jackyurchenco@gmail.com>
2026-05-14 21:38:20 +0200

    feat(auth): add jwt-auth.guard

---
+++ libs/auth/src/lib/infrastructure/guards/jwt-auth.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+
+  handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
+
+    if (err || !user) {
+      throw err || new UnauthorizedException('Користувач не авторизований');
+    }
+
+    return user;
+  }
+}
