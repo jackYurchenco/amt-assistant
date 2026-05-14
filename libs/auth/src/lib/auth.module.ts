52a6d8ba6c35d4a1325208ba66b461b248a529d1
eugene.yurchenko <jackyurchenco@gmail.com>
2026-05-14 21:44:13 +0200

    feat(auth): implement JWT authentication

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,22 +1,30 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './interface/auth.controller';
 import { UtilCryptoModule } from '@amt-assistant/util-crypto';
 import { UtilTokenModule } from '@amt-assistant/util-token';
 import { LoginUseCase } from './application/login.use-case';
 import { UsersModule } from '@amt-assistant/users';
+import { JwtStrategy } from './infrastructure/jwt.strategy';
+import { JwtAuthGuard } from './infrastructure/guards/jwt-auth.guard';
 
 @Module({
   imports: [
     UsersModule,
     UtilCryptoModule,
     UtilTokenModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
   ],
   controllers: [
     AuthController,
   ],
   providers: [
     LoginUseCase,
+    JwtStrategy,
+    JwtAuthGuard,
   ],
-  exports: [],
+  exports: [
+    JwtAuthGuard,
+  ],
 })
 export class AuthModule {}
