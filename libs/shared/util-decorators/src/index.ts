1aedfd09d13052b9ee21eb9e3fe2cc26c515f15c
eugene.yurchenko <jackyurchenco@gmail.com>
2026-05-14 21:59:10 +0200

    feat(util-decorators): add public decorator

--- libs/shared/util-decorators/src/index.ts
+++ libs/shared/util-decorators/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/trim.decorator';
 export * from './lib/to-lower-case.decorator';
 export * from './lib/authenticated-user-id.decorator';
+export * from './lib/public.decorator';
---
+++ libs/shared/util-decorators/src/lib/public.decorator.ts
@@ -0,0 +1,6 @@
+import { CustomDecorator, SetMetadata } from '@nestjs/common';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export function Public(): CustomDecorator<string> {
+  return SetMetadata(IS_PUBLIC_KEY, true);
+}
