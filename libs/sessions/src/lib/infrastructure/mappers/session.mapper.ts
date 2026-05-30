bcd016990425a5708be993e757758cc8475627f1
eugene.yurchenko <jackyurchenco@gmail.com>
2026-05-30 06:59:36 +0200

    feat(sessions): add session mapper

---
+++ libs/sessions/src/lib/infrastructure/mappers/session.mapper.ts
@@ -0,0 +1,24 @@
+import { Session as PrismaSession } from '@prisma/client';
+import { SessionEntity } from '../../domain/session.entity';
+
+export class SessionMapper {
+  static toDomain(raw: PrismaSession): SessionEntity {
+    return SessionEntity.restore({
+      id: raw.id,
+      userId: raw.userId,
+      refreshToken: raw.refreshToken,
+      expiresAt: raw.expiresAt,
+      userAgent: raw.userAgent,
+    });
+  }
+
+  static toPersistence(session: SessionEntity): PrismaSession {
+    return {
+      id: session.id.getValue(),
+      userId: session.userId.getValue(),
+      // TODO refreshToken: session.refreshToken.getValue(),
+      expiresAt: session.expiresAt,
+      userAgent: session.userAgent,
+    };
+  }
+}
