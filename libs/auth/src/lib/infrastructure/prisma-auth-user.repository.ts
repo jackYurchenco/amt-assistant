4d4659da9c2b72e2ab8f593c629f6349704b13a9
eugene.yurchenko <jackyurchenco@gmail.com>
2026-06-05 09:27:05 +0200

    feat(auth): add PrismaAuthUserRepository

---
+++ libs/auth/src/lib/infrastructure/prisma-auth-user.repository.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@amt-assistant/prisma';
+import { AuthUserReader } from '../domain/ports/auth-user.reader';
+import { AuthUser } from '../domain/auth-user.entity';
+
+@Injectable()
+export class PrismaAuthUserRepository implements AuthUserReader {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getUserByEmail(email: string): Promise<AuthUser | null> {
+    const raw = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (!raw) {
+      return null;
+    }
+
+    return AuthUser.restore({
+      id: raw.id,
+      email: raw.email,
+      passwordHash: raw.passwordHash,
+    });
+  }
+}
