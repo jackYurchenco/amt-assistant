35b4c5d83e9930c209ab59119bf35b6b4f92088e
eugene.yurchenko <jackyurchenco@gmail.com>
2026-06-06 09:07:19 +0200

    feat(sessions): add removeAllByUserId method to SessionRemover port

--- libs/sessions/src/lib/domain/ports/session-remover.port.ts
+++ libs/sessions/src/lib/domain/ports/session-remover.port.ts
@@ -1,5 +1,6 @@
-import { SessionId } from '@amt-assistant/domain';
+import { SessionId, UserId } from '@amt-assistant/domain';
 
 export abstract class SessionRemover {
-  abstract remove(id: SessionId): Promise<void>
+  abstract remove(id: SessionId): Promise<void>;
+  abstract removeAllByUserId(id: UserId): Promise<void>;
 }
