ac85314be2102740cd58d624cdaae89c79cfd197
eugene.yurchenko <jackyurchenco@gmail.com>
2026-06-06 09:34:24 +0200

    feat(sessions): add userId to RemoveSessionByIdCommand

--- libs/sessions/src/lib/application/remove-session-by-id/remove-session-by-id.command.ts
+++ libs/sessions/src/lib/application/remove-session-by-id/remove-session-by-id.command.ts
@@ -1,3 +1,6 @@
 export class RemoveSessionByIdCommand {
-  constructor(public readonly sessionId: string) {}
+  constructor(
+    public readonly sessionId: string,
+    public readonly userId: string,
+  ) {}
 }
