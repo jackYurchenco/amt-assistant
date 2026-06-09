534fe2d7c51dc603cba2e421c5c818ab1434b95d
eugene.yurchenko <jackyurchenco@gmail.com>
2026-06-09 12:39:33 +0200

    feat(sessions): implement get sessions by user id in controller

--- libs/sessions/src/lib/interface/sessions.controller.ts
+++ libs/sessions/src/lib/interface/sessions.controller.ts
@@ -1,6 +1,26 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, HttpStatus } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { GetSessionsByUserIdUseCase } from '../application/get-sessions-by-user-id/get-sessions-by-user-id.use-case';
+import { GetSessionsByUserIdQuery } from '../application/get-sessions-by-user-id/get-sessions-by-user-id.query';
+import { SessionResponseDto } from './dto/session-response.dto';
+import { GetSessionsByUserIdDto } from './dto/get-sessions-by-user-id.dto';
 
-@Controller('sessions')
 @ApiTags('sessions')
-export class SessionsController {}
+@ApiBearerAuth()
+@Controller('sessions')
+export class SessionsController {
+  constructor(private readonly getSessionsByUserIdUseCase: GetSessionsByUserIdUseCase) {}
+
+  @Get('user/:userId')
+  @ApiOperation({ summary: 'Get sessions by user id' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The sessions have been successfully retrieved.',
+    type: [SessionResponseDto],
+  })
+  async getSessionsByUserId(@Param() dto: GetSessionsByUserIdDto): Promise<SessionResponseDto[]> {
+    const query = new GetSessionsByUserIdQuery(dto.userId);
+    const sessions = await this.getSessionsByUserIdUseCase.execute(query);
+    return sessions.map(session => SessionResponseDto.fromEntity(session));
+  }
+}
