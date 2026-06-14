e4f980bbc6d22455c974f521f5957b25c2643559
eugene.yurchenko <jackyurchenco@gmail.com>
2026-06-14 21:10:52 +0200

    refactor(sessions): update remove-sessions-by-user-id imports in module

--- libs/sessions/src/lib/sessions.module.ts
+++ libs/sessions/src/lib/sessions.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { SessionsController } from './interface/sessions.controller';
 import { PrismaModule } from '@amt-assistant/prisma';
 import { GetSessionsByUserIdUseCase } from './application/get-sessions-by-user-id/get-sessions-by-user-id.use-case';
-import { RemoveAllSessionsByUserIdUseCase } from './application/remove-all-sessions-by-user-id/remove-all-sessions-by-user-id.use-case';
+import { RemoveSessionsByUserIdUseCase } from './application/remove-sessions-by-user-id/remove-sessions-by-user-id.use-case';
 import { RemoveSessionByIdUseCase } from './application/remove-session-by-id/remove-session-by-id.use-case';
 import { SessionReader } from './domain/ports/session-reader.port';
 import { SessionRemover } from './domain/ports/session-remover.port';
@@ -13,7 +13,7 @@ import { PrismaSessionRepository } from './infrastructure/prisma-session.reposit
   controllers: [SessionsController],
   providers: [
     GetSessionsByUserIdUseCase,
-    RemoveAllSessionsByUserIdUseCase,
+    RemoveSessionsByUserIdUseCase,
     RemoveSessionByIdUseCase,
     {
       provide: SessionReader,
