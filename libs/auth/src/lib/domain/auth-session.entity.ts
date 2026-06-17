51ca960f92fd15eef1177c3c291693d89cbd3453
eugene.yurchenko <jackyurchenco@gmail.com>
2026-06-17 09:16:00 +0200

    feat(auth): add AuthSession entity

---
+++ libs/auth/src/lib/domain/auth-session.entity.ts
@@ -0,0 +1,43 @@
+import { UserId, SessionId, RefreshToken } from '@amt-assistant/domain';
+
+export class AuthSession {
+  private constructor(
+    public readonly id: SessionId,
+    public readonly userId: UserId,
+    public readonly refreshToken: RefreshToken,
+    public readonly expiresAt: Date,
+    public readonly userAgent?: string,
+  ) {}
+
+  static create(props: {
+    id: string;
+    userId: string;
+    refreshToken: string;
+    expiresAt: Date;
+    userAgent?: string;
+  }): AuthSession {
+    return new AuthSession(
+      SessionId.create(props.id),
+      UserId.create(props.userId),
+      RefreshToken.create(props.refreshToken),
+      props.expiresAt,
+      props.userAgent,
+    );
+  }
+
+  static restore(props: {
+    id: string;
+    userId: string;
+    refreshToken: string;
+    expiresAt: Date;
+    userAgent?: string;
+  }): AuthSession {
+    return new AuthSession(
+      SessionId.create(props.id),
+      UserId.create(props.userId),
+      RefreshToken.create(props.refreshToken),
+      props.expiresAt,
+      props.userAgent,
+    );
+  }
+}
