dddf88d72d9ca8ffa2e05da2cf86f202966257f3
eugene.yurchenko <jackyurchenco@gmail.com>
2026-06-22 11:09:10 +0200

    refactor(util-token): export TokenGenerationException

--- libs/shared/util-token/src/index.ts
+++ libs/shared/util-token/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/services/token.service';
 export * from './lib/interfaces/token-payload.interface';
 export * from './lib/interfaces/auth-tokens.interface';
 export * from './lib/interfaces/jwt-payload.interface';
+export * from './lib/exceptions/token-generation.exception';
