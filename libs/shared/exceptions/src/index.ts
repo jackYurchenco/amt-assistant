27c3be1465d315bd1646d0eb17113624553efa59
eugene.yurchenko <jackyurchenco@gmail.com>
2026-06-22 11:21:35 +0200

    refactor(exceptions): update public api exports for new structure

--- libs/shared/exceptions/src/index.ts
+++ libs/shared/exceptions/src/index.ts
@@ -1,3 +1,4 @@
-export * from './lib/domain.exception';
-export * from './lib/application.exception';
-export * from './lib/infrastructure.exception';
+export * from './lib/base/domain.exception';
+export * from './lib/base/application.exception';
+export * from './lib/base/infrastructure.exception';
+export * from './lib/infrastructure/database-operation.exception';
