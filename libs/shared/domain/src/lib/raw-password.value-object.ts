21c335d2cd6228d11e184685fcc31573d7b58acd
eugene.yurchenko <jackyurchenco@gmail.com>
2026-06-27 19:25:06 +0200

    refactor(domain): use DomainValidationException in RawPassword value object

--- libs/shared/domain/src/lib/raw-password.value-object.ts
+++ libs/shared/domain/src/lib/raw-password.value-object.ts
@@ -1,18 +1,18 @@
 import { BaseValueObject } from './base.value-object';
-import { BadRequestException } from '@nestjs/common';
+import { DomainValidationException } from '@amt-assistant/exceptions';
 
 export class RawPassword extends BaseValueObject<string, 'RawPassword'> {
   public static create(password: string): RawPassword {
     if (!password || password.trim() === '') {
-      throw new BadRequestException('The password cannot be empty');
+      throw new DomainValidationException('The password cannot be empty');
     }
 
     if (password.length > 64) {
-      throw new BadRequestException('The password cannot be longer than 64 characters');
+      throw new DomainValidationException('The password cannot be longer than 64 characters');
     }
 
     if (password.length < 6) {
-      throw new BadRequestException('The password must be at least 6 characters long.');
+      throw new DomainValidationException('The password must be at least 6 characters long.');
     }
 
     return new RawPassword(password);
