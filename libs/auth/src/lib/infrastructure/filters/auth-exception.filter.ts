b532009259a94c4b2c6a67e0f7e5deca29e1fdc4
eugene.yurchenko <jackyurchenco@gmail.com>
2026-06-28 10:39:02 +0200

    refactor(auth): handle InvalidTokenException in AuthExceptionFilter

--- libs/auth/src/lib/infrastructure/filters/auth-exception.filter.ts
+++ libs/auth/src/lib/infrastructure/filters/auth-exception.filter.ts
@@ -1,6 +1,7 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpStatus } from '@nestjs/common';
 import { Response } from 'express';
 import { InvalidCredentialsException } from '../../application/exceptions/invalid-credentials.exception';
+import { InvalidTokenException } from '../../application/exceptions/invalid-token.exception';
 import { DomainException, InfrastructureException, ApplicationException } from '@amt-assistant/exceptions';
 
 @Catch(ApplicationException, DomainException, InfrastructureException)
@@ -11,7 +12,7 @@ export class AuthExceptionFilter implements ExceptionFilter {
 
     let status = HttpStatus.INTERNAL_SERVER_ERROR;
 
-    if (exception instanceof InvalidCredentialsException) {
+    if (exception instanceof InvalidCredentialsException || exception instanceof InvalidTokenException) {
       status = HttpStatus.UNAUTHORIZED;
     } else if (exception instanceof DomainException) {
       status = HttpStatus.BAD_REQUEST;
