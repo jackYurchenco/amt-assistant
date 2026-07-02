7a89c111d2c77aeec81caa02286ec46a510c8fac
eugene.yurchenko <jackyurchenco@gmail.com>
2026-07-02 14:15:36 +0200

    feat(util-token): export TokenVerificationException

--- libs/shared/util-token/src/index.ts
+++ libs/shared/util-token/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/interfaces/token-payload.interface';
 export * from './lib/interfaces/auth-tokens.interface';
 export * from './lib/interfaces/jwt-payload.interface';
 export * from './lib/exceptions/token-generation.exception';
+export * from './lib/exceptions/token-verification.exception';
