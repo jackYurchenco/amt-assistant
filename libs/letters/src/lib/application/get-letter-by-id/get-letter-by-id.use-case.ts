bbf20bc06a5c7ef70071e39f8932696e2d922a9c
eugene.yurchenko <jackyurchenco@gmail.com>
2026-07-04 16:29:48 +0200

    refactor(letters): use custom exceptions in GetLetterByIdUseCase

--- libs/letters/src/lib/application/get-letter-by-id/get-letter-by-id.use-case.ts
+++ libs/letters/src/lib/application/get-letter-by-id/get-letter-by-id.use-case.ts
@@ -1,8 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { GetLetterByIdQuery } from './get-letter-by-id.query';
 import { Letter } from '../../domain/letter.entity';
 import { LetterId } from '@amt-assistant/domain';
 import { LetterReader } from '../../domain/ports/letter-reader.port';
+import { LetterNotFoundException } from '../exceptions/letter-not-found.exception';
 
 @Injectable()
 export class GetLetterByIdUseCase {
@@ -12,7 +13,7 @@ export class GetLetterByIdUseCase {
     const letter = await this.letterReader.findById(LetterId.create(query.id));
 
     if (!letter) {
-      throw new NotFoundException(`Letter with ID ${query.id} not found`);
+      throw new LetterNotFoundException(`Letter with ID ${query.id} not found`);
     }
 
     return letter;
