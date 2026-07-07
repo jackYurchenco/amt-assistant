e57828a87bf1b9fc7029fd395346ccf680eb26f7
eugene.yurchenko <jackyurchenco@gmail.com>
2026-07-07 16:21:55 +0200

    feat(users): throw exception if user not found in GetUserByIdUseCase

--- libs/users/src/lib/application/get-user-by-id/get-user-by-id.use-case.ts
+++ libs/users/src/lib/application/get-user-by-id/get-user-by-id.use-case.ts
@@ -3,12 +3,19 @@ import { GetUserByIdQuery } from './get-user-by-id.query';
 import { User } from '../../domain/user.entity';
 import { UserId } from '@amt-assistant/domain';
 import { UserReader } from '../../domain/ports/user-reader.port';
+import { UserNotFoundException } from '../exceptions/user-not-found.exception';
 
 @Injectable()
 export class GetUserByIdUseCase {
   constructor(private readonly userReader: UserReader) {}
 
-  async execute(query: GetUserByIdQuery): Promise<User | null> {
-    return this.userReader.findById(UserId.create(query.id));
+  async execute(query: GetUserByIdQuery): Promise<User> {
+    const user = await this.userReader.findById(UserId.create(query.id));
+
+    if (!user) {
+      throw new UserNotFoundException(`User with ID ${query.id} not found`);
+    }
+
+    return user;
   }
 }
