17ddd4e630e964d41be5c26921bad9c29e54eaa2
eugene.yurchenko <jackyurchenco@gmail.com>
2026-07-31 06:59:39 +0200

    refactor(users): use generic NotFoundException in GetUserByIdUseCase

--- libs/users/src/lib/application/get-user-by-id/get-user-by-id.use-case.ts
+++ libs/users/src/lib/application/get-user-by-id/get-user-by-id.use-case.ts
@@ -3,7 +3,7 @@ import { GetUserByIdQuery } from './get-user-by-id.query';
 import { User } from '../../domain/user.entity';
 import { UserId } from '@amt-assistant/domain';
 import { UserReader } from '../../domain/ports/user-reader.port';
-import { UserNotFoundException } from '../exceptions/user-not-found.exception';
+import { NotFoundException } from '@amt-assistant/exceptions';
 
 @Injectable()
 export class GetUserByIdUseCase {
@@ -13,7 +13,7 @@ export class GetUserByIdUseCase {
     const user = await this.userReader.findById(UserId.create(query.id));
 
     if (!user) {
-      throw new UserNotFoundException(`User with ID ${query.id} not found`);
+      throw new NotFoundException(`User with ID ${query.id} not found`);
     }
 
     return user;
