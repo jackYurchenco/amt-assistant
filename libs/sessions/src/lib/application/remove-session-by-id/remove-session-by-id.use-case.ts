6ca70f5f16f7c69f0351e26c43d755d16ea895a1
eugene.yurchenko <jackyurchenco@gmail.com>
2026-08-04 07:39:07 +0200

    refactor(sessions): use generic exceptions in RemoveSessionByIdUseCase

--- libs/sessions/src/lib/application/remove-session-by-id/remove-session-by-id.use-case.ts
+++ libs/sessions/src/lib/application/remove-session-by-id/remove-session-by-id.use-case.ts
@@ -3,8 +3,7 @@ import { RemoveSessionByIdCommand } from './remove-session-by-id.command';
 import { SessionRemover } from '../../domain/ports/session-remover.port';
 import { SessionReader } from '../../domain/ports/session-reader.port';
 import { SessionId } from '@amt-assistant/domain';
-import { SessionNotFoundException } from '../exceptions/session-not-found.exception';
-import { SessionForbiddenException } from '../exceptions/session-forbidden.exception';
+import { ForbiddenException, NotFoundException } from '@amt-assistant/exceptions';
 
 @Injectable()
 export class RemoveSessionByIdUseCase {
@@ -18,11 +17,11 @@ export class RemoveSessionByIdUseCase {
     const session = await this.sessionReader.findById(sessionId);
 
     if (!session) {
-      throw new SessionNotFoundException(`Session with ID ${command.sessionId} not found`);
+      throw new NotFoundException(`Session with ID ${command.sessionId} not found`);
     }
 
     if (session.userId.getValue() !== command.userId) {
-      throw new SessionForbiddenException('You are not allowed to delete this session');
+      throw new ForbiddenException('You are not allowed to delete this session');
     }
 
     await this.sessionRemover.remove(sessionId);
