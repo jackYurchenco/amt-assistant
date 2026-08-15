929e213a7bb05d4fd66983798432510e39cca9f0
eugene.yurchenko <jackyurchenco@gmail.com>
2026-08-15 08:24:05 +0200

    feat(documents): update DocumentsModule with new providers and controller

--- libs/documents/src/lib/documents.module.ts
+++ libs/documents/src/lib/documents.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '@amt-assistant/prisma';
+import { DocumentsController } from './interface/documents.controller';
+import { UploadDocumentUseCase } from './application/upload-document/upload-document.use-case';
+import { PrismaDocumentRepository } from './infrastructure/prisma-document.repository';
+import { LocalStorageService } from './infrastructure/local-storage.service';
+import { DocumentWriter } from './domain/ports/document-writer.port';
+import { StorageWriter } from './domain/ports/storage-writer.port';
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: [],
+  imports: [PrismaModule],
+  controllers: [DocumentsController],
+  providers: [
+    UploadDocumentUseCase,
+    {
+      provide: DocumentWriter,
+      useClass: PrismaDocumentRepository,
+    },
+    {
+      provide: StorageWriter,
+      useClass: LocalStorageService,
+    },
+  ],
+  exports: [UploadDocumentUseCase],
 })
 export class DocumentsModule {}
