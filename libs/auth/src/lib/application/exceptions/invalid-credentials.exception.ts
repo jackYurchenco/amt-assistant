31b1901e73faa2e29601c91d6354edc8fa5af790
eugene.yurchenko <jackyurchenco@gmail.com>
2026-09-02 07:05:15 +0200

    refactor(auth): remove invalid-credentials.exception.ts

--- libs/auth/src/lib/application/exceptions/invalid-credentials.exception.ts
+++
@@ -1,7 +0,0 @@
-import { ApplicationException } from '@amt-assistant/exceptions';
-
-export class InvalidCredentialsException extends ApplicationException {
-  constructor() {
-    super('Invalid credentials');
-  }
-}
