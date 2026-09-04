b40ffb10ba3bf26178414ecc4e613c255d86bea7
eugene.yurchenko <jackyurchenco@gmail.com>
2026-09-04 06:17:08 +0200

    refactor(auth): remove invalid-token.exception.ts

--- libs/auth/src/lib/application/exceptions/invalid-token.exception.ts
+++
@@ -1,7 +0,0 @@
-import { ApplicationException } from '@amt-assistant/exceptions';
-
-export class InvalidTokenException extends ApplicationException {
-  constructor(message = 'User is not authorized or token is invalid.') {
-    super(message);
-  }
-}
