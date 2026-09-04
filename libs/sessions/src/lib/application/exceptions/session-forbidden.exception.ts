176a2e4a1da1931f23b65da0e555aadc37b5e843
eugene.yurchenko <jackyurchenco@gmail.com>
2026-09-04 06:42:11 +0200

    refactor(sessions): remove session-forbidden.exception.ts

--- libs/sessions/src/lib/application/exceptions/session-forbidden.exception.ts
+++
@@ -1,7 +0,0 @@
-import { ApplicationException } from '@amt-assistant/exceptions';
-
-export class SessionForbiddenException extends ApplicationException {
-  constructor(message = 'You are not allowed to perform this action on this session.') {
-    super(message);
-  }
-}
