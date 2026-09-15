bb5493a6f6f6e9bbe3ba0d0bdd95f2e791d03e6d
eugene.yurchenko <jackyurchenco@gmail.com>
2026-09-15 07:33:06 +0200

    feat(documents): register GetDocumentByIdUseCase and DocumentReader in DocumentsModule

--- libs/documents/src/lib/documents.module.ts
+++ libs/documents/src/lib/documents.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '@amt-assistant/prisma';
 import { DocumentsController } from './interface/documents.controller';
 import { UploadDocumentUseCase } from './application/upload-document/upload-document.use-case';
+import { GetDocumentByIdUseCase } from './application/get-document-by-id/get-document-by-id.use-case';
 import { PrismaDocumentRepository } from './infrastructure/prisma-document.repository';
 import { LocalStorageService } from './infrastructure/local-storage.service';
 import { DocumentWriter } from './domain/ports/document-writer.port';
+import { DocumentReader } from './domain/ports/document-reader.port';
 import { StorageWriter } from './domain/ports/storage-writer.port';
 
 @Module({
@@ -12,8 +14,13 @@ import { StorageWriter } from './domain/ports/storage-writer.port';
   controllers: [DocumentsController],
   providers: [
     UploadDocumentUseCase,
+    GetDocumentByIdUseCase,
     {
       provide: DocumentWriter,
+      useClass: PrismaDocumentRepository,
+    },
+    {
+      provide: DocumentReader,
       useClass: PrismaDocumentRepository,
     },
     {
@@ -21,6 +28,6 @@ import { StorageWriter } from './domain/ports/storage-writer.port';
       useClass: LocalStorageService,
     },
   ],
-  exports: [UploadDocumentUseCase],
+  exports: [UploadDocumentUseCase, GetDocumentByIdUseCase],
 })
 export class DocumentsModule {}
